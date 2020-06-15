c3c0161c8fe23737b216ec6cb5f89b5092138d16
mithun-mani <65134878+mithun-mani@users.noreply.github.com>
2020-06-15 16:22:02 -0400

    Update gputest.cu

--- gputest.cu
+++ gputest.cu
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <math.h>
 #include <stdio.h>
-using namespace std;
 
 __global__ void cuda_hello(){
     printf("Hello World from GPU!\n");
@@ -11,4 +9,3 @@ int main() {
     cuda_hello<<<1,1>>>(); 
     return 0;
 }
-
