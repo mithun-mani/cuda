e10ba26624823849be758ae60e2724100a147906
mithun-mani <65134878+mithun-mani@users.noreply.github.com>
2020-06-15 15:57:29 -0400

    Add files via upload

---
+++ gputest.cu.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+__global__
+void add (int n, float *x, float *y){
+    for (int i = 0; i < n; i++){
+        y[i] = x[i] + y[i];
+    }
+}
+int main(void) {
+    int N = 1;
+
+    float *x;
+    float *y;
+
+    cudaMallocManaged(&x, N*sizeof(float));
+    cudaMallocManaged(&y, N*sizeof(float));
+
+
+    for (int i = 0; i < N; i++){
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    add<<<1,1>>>(N,x,y);
+    cout << y;
+
+    cudaDeviceSynchronize();
+
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
