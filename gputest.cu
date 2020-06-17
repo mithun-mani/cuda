1bc9abce6dd280fad458cef4f2e79d395cd56cfe
mithun-mani <65134878+mithun-mani@users.noreply.github.com>
2020-06-16 20:01:51 -0400

    Update gputest.cu

--- gputest.cu
+++ gputest.cu
@@ -1,4 +1,12 @@
 #include <stdio.h>
-int main() {
+/*int main() {
     printf("Hello World from GPU!\n");
+}
+*/
+__global__
+void printer (int n, float *x, float *y){
+    printf("Hello World from GPU\n");
+}
+int main(void) {
+    printer<<<1,1>>>();
 }
