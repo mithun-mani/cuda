e9af7ed9ffe5d3ef514e59ef385a56947a3604fc
mithun-mani <65134878+mithun-mani@users.noreply.github.com>
2020-07-27 16:58:03 -0400

    Update vectoradd2.cu

--- vectoradd2.cu
+++ vectoradd2.cu
@@ -5,7 +5,9 @@
 __global__
 void add(int n, float *x, float *y)
 {
-  for (int i = 0; i < n; i++)
+  int index = threadIdx.x;
+  int stride = blockDim.x;
+  for (int i = index; i < n; i+=stride)
     y[i] = x[i] + y[i];
 }
 
